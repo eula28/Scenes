e0ea0ff5148cead6d860059e2d4de42e3555cb3e
Genesis Gregorio <genesisgregorio282001@gmail.com>
2024-05-15 18:43:15 +0800

    Finish Feedback Functionality

---
+++ AR and Map/IR-Maps Scripts/ToFeedback.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ToFeedback : MonoBehaviour
+{
+    // The name of the scene you want to load
+    public string toFeedback;
+
+    public void OnMouseUpAsButton()
+    {
+        // Load the specified scene
+        SceneManager.LoadScene(toFeedback);
+    }
+}
