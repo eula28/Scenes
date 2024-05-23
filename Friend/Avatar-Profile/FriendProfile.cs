d30f7887bdfa2c2bab5946f206c7c605dce0f52e
arniemonica <arniemonidelrosario@gmail.com>
2024-05-23 19:27:46 +0800

    Change

---
+++ Friend/Avatar-Profile/FriendProfile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendProfile : MonoBehaviour
+{
+    public ProfileDatabase profileDB;
+    public SpriteRenderer profileSprite;
+}
--- Friend/Avatar-Profile/Profile.cs
+++ Friend/Avatar-Profile/Profile.cs
@@ -6,6 +6,5 @@ using UnityEngine;
 [System.Serializable]
 public class Profile
 {
-    public string profileName;
     public Sprite profileSprite;
 }
