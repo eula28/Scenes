d9b1dc93aebcbaf3c7ee5a43e15266c1e01fa3b7
Alex13Costa <ajrscosta13@gmail.com>
2024-05-29 16:23:40 +0800

    starting

--- Feedback/Script/AdminScript.cs
+++ Feedback/Script/AdminScript.cs
@@ -1,18 +1,24 @@
+using Firebase.Firestore;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AdminScript : MonoBehaviour
 {
+    FirebaseFirestore db;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // Check if the FirebaseController instance exists
+        if (FirebaseController.Instance != null)
+        {
+            db = FirebaseFirestore.DefaultInstance;
+        }
+        else
+        {
+            Debug.LogError("FirebaseController instance not found.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
